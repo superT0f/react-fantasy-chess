8542128d7867fcb7d211995474ae3e2c60571568
SuperT0f <christophe.prigent2@gmail.com>
2025-08-06 21:21:54 +0200

    feat(footer):new basic footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      
+      <p>© 2025 Fantasy Chess. <a href="https://gitlab.com/SuperT0f/react-fantasy-chess/-/blob/main/LICENCE.md">GNU GPL V3</a>.</p>
+      <p>made with <span className="heart">♥</span>, my brain and deepseek. sources on <a href="https://gitlab.com/SuperT0f/react-fantasy-chess" target="_blank">
+      GitLab</a>.</p>
+    </div>
+  );
+};
+
+export default Footer;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Logo from './components/Logo';
 import Game from './Game';
+import Footer from './components/Footer';
 import "./styles.css";
 
 const root = createRoot(document.getElementById("root"));
@@ -9,5 +10,6 @@ root.render(
   <StrictMode>
     <Logo />
     <Game />
+    <Footer />
   </StrictMode>
 );
