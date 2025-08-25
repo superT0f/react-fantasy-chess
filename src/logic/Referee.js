932c174970b6760c3987bf08824645d13e013a39
SuperT0f <christophe.prigent2@gmail.com>
2025-08-25 11:13:25 +0200

    feat(referee): restore getInitialBoard to  default chess board

--- src/logic/Referee.js
+++ src/logic/Referee.js
@@ -35,7 +35,17 @@ export default class Referee {
       'P', 'P', 'P', '', 'P', 'P', 'P', 'P',
       'R', 'N', 'B', '', 'K', '', 'N', 'R'
     ];
-    return checkMate;
+    const defaultBoard = [
+      'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r',
+      'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
+      '', '', '', '', '', '', '', '',
+      '', '', '', '', '', '', '', '',
+      '', '', '', '', '', '', '', '',
+      '', '', '', '', '', '', '', '',
+      'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
+      'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'
+    ];
+    return defaultBoard;
   }
 
   reset() {
